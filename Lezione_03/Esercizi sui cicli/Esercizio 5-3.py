a4ea8aa6042c58186f31fbbd61dcce6b75df08c5
Fradala04 <francydalascio@gmail.com>
2025-02-28 19:09:44 +0100

    a

---
+++ Lezione_03/Esercizi sui cicli/Esercizio 5-3.py
@@ -0,0 +1,8 @@
+# alien_color:str = "green"
+
+# if alien_color == "green":
+#     print("The player just earnt 5 points")
+
+alien_color:str = "yellow"
+if alien_color == "green":
+    print("The player just earnt 5 points")
