9c61f7e69b5465db72123e5bc8ad8ace45074f4b
Fradala04 <francydalascio@gmail.com>
2025-03-14 16:22:08 +0100

    a

--- Lezione_03/Esercizi sui cicli/Esercizio 5-1.py
+++
@@ -1,7 +0,0 @@
-car = 'subaru'
-if car == 'subaru':
-    print("Is car == 'subaru'? I predict True.")
-    print(car == 'subaru')
-else:
-    if print("\nIs car == 'audi'? I predict False."):
-        print(car == 'audi')
